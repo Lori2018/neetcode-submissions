ed5d742ab99d1342bf27342f3b813cc50f46c2dc
Lori Brown <34607662+Lori2018@users.noreply.github.com>
2026-07-31 16:54:16 -0400

    Add: eating-bananas - submission-3

---
+++ Data Structures & Algorithms/eating-bananas/submission-3.py
@@ -0,0 +1,19 @@
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        def hours(rate):
+            res = 0
+            for p in piles:
+                res += math.ceil(p/rate)
+            return res
+        # range starts at [1, max in piles]
+        left = 1
+        right = max(piles)
+        while left < right:
+            print(left, right)
+            mid = (left + right) // 2
+            cur_h = hours(mid)
+            if cur_h > h: # need to increase rate
+                left = mid+1
+            elif cur_h <= h:
+                right = mid
+        return left
